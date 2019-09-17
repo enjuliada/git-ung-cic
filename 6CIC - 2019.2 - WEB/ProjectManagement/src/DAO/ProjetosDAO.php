b48552697c149f7816829b972378c9cf72ca3758
joaoortiz <48797718+joaoortiz@users.noreply.github.com>
2019-09-17 13:57:12 -0300

    Update ProjetosDAO.php

--- 6CIC - 2019.2 - WEB/ProjectManagement/src/DAO/ProjetosDAO.php
+++ 6CIC - 2019.2 - WEB/ProjectManagement/src/DAO/ProjetosDAO.php
@@ -80,7 +80,7 @@ class ProjetosDAO {
         
         $vConn = ConexaoDAO:: abrirConexao();
         
-        $sqlProj = "Select * from Projetos P, Equipes E, Usuarios U where ";
+        $sqlProj = "Select * from Projetos P where ";
         $sqlProj .= "P.codigo_PROJETO = '$tmpCodigo'";
         //$sqlProj .= "P.codigo_PROJETO = E.codigoProjeto_EQUIPE and ";
         //$sqlProj .= "E.emailUsuario_EQUIPE = U.email_USUARIO";
