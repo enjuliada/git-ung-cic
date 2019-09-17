887bd13cf83053511a312aa00ef9d9a0b09bb9fe
joaoortiz <48797718+joaoortiz@users.noreply.github.com>
2019-09-17 13:21:59 -0300

    Add files via upload

---
+++ 6CIC - 2019.2 - WEB/ProjectManagement/src/UI/DetalhesProjetoUI.php
@@ -0,0 +1,105 @@
+<meta charset="utf-8">
+
+<?php
+session_start();
+?>
+<html>
+    <head>
+
+        <link rel="stylesheet" href="../css/bootstrap.min.css">
+
+    </head>
+    <body>
+
+        <div class="container" style="margin-top: 10px;">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="card">
+                        <div class="card-header bg-primary text-white">
+                            NOME DO PROJETO 
+                        </div>
+                        <div class="card-body">
+                            descricao
+                        </div>                               
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-4" style="margin-top:10px;">
+
+                    <div class="card">
+                        <div class="card-header">
+                            <h5 class="card-title">Concluído</h5>
+                        </div>
+                        <div class="card-body text-center" style="height:120px;line-height:80px;">
+                            <font style="font-family:impact;font-size:100px;">
+                            0%
+                            </font>                            
+                        </div>
+                    </div>
+                    <div class="card" style="margin-top: 10px;">
+                        <div class="card-header">
+                            <h5 class="card-title">Equipe</h5>
+                        </div>
+                        <div class="card-body text-center" style="min-height:220px;height:auto;">
+                            Pedro Leão<br>
+                            Felipe Fornaciari<br>
+                            Matheus Noronha
+                            
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-8" style="margin-top:10px;">
+                    <div style="width:30%;height:60px;background-color:lightgreen;" >
+
+                    </div>
+                    <div class="float-left">
+                        12/08/2019
+                    </div>
+                    <div class="float-right">     
+                        15/11/2019
+                    </div>
+                    <br><br>
+                    <div class="card">
+                        <div class="card-header bg-danger text-white">
+                            Tarefas
+                        </div>
+                        <div class="card-body" style="min-height:318px;height:auto;">
+                            descricao
+                        </div>  
+
+                    </div>
+                </div>
+            </div>
+
+        </div>
+
+    </body>
+
+    ?>
+
+</html>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
