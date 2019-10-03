21aa8c2dbb2356d4f97d90df57014713b497e5a4
joaoortiz <48797718+joaoortiz@users.noreply.github.com>
2019-10-02 21:38:52 -0300

    Update Candidatos.cpp

--- 2CIC - 2019.2 - Prog/Aulas/Aula8/Candidatos.cpp
+++ 2CIC - 2019.2 - Prog/Aulas/Aula8/Candidatos.cpp
@@ -4,124 +4,127 @@
 #include<string.h>
 
 int main(){
-	//declaração de variaveis;
-	int numCand, tEleit = 0;
+	 //declaraÃ§Ã£o de variaveis
+	int numCand, totEleit = 0;
 	int qtCand1 = 0, qtCand2 = 0, qtCand3 = 0,
-		qtCand4 = 0, qtNulo = 0, qtBran = 0;
-	float pCand1, pCand2, pCand3, pCand4, pNulo, pBran;
+	    qtCand4 = 0, qtBran = 0, qtNulo = 0;
+	float pCand1, pCand2, pCand3, pCand4,
+		pBran, pNulo;
 	char nome[20];
 	int i;
-	
-	setlocale(LC_ALL,"");
-	
+	 	 
+	setlocale(LC_ALL, "");
+	 
 	do{
-		printf("**** PROGRAMA ELEIÇÃO 2019****\n\n");
+	 
+		printf("**** PROGRAMA ELEIÃ‡ÃƒO ****\n\n");
 		
-		printf("Informe o número do candidato:\n\n");
-		printf("Número\t\t\tCandidato\n");
-		printf("----------------------------------------\n");
-		printf("1\t\t\tOrtiz\n");
-		printf("2\t\t\tUlhmann\n");
-		printf("3\t\t\tHeredia\n");
-		printf("4\t\t\tAlbuquerque\n");
+		printf("Informe o nÃºmero do candidato: \n\n");
+		printf("Legenda\t\tCandidato\n");
+		printf("------------------------------------\n");
+		printf("1\t\t\tORTIZ\n");
+		printf("2\t\t\tHEREDIA\n");
+		printf("3\t\t\tALBUQUERQUE\n");
+		printf("4\t\t\tULHMANN\n");
 		printf("0\t\t\tNulo\n");
-		printf("(Qualquer valor)\tBranco\n\n");
+		printf("(Qualquer valor)\t\t\tBranco\n\n");
 		
 		printf("Seu voto: ");
 		scanf("%d", &numCand);
 		
-		if(numCand == 1){
-			strcpy(nome,"Ortiz"); //nome = "Ortiz";
-			qtCand1++; //qtCand1 = qtCand1 + 1;
-			tEleit++;
-			
+		if(numCand == 0){
+			qtNulo++; //qtNulo = qtNulo + 1;			
+			totEleit++;
+			strcpy(nome, "**NULO**");
+		}else if(numCand == 1){
+			qtCand1++;
+			totEleit++;
+			strcpy(nome, "**ORTIZ**");
 		}else if(numCand == 2){
-			strcpy(nome,"Ulhmann"); 
-			qtCand2++; 
-			tEleit++;
-			
+			qtCand2++;
+			totEleit++;
+			strcpy(nome, "**HEREDIA**");
 		}else if(numCand == 3){
-			strcpy(nome,"Heredia"); 
-			qtCand3++; 
-			tEleit++;
-			
+			qtCand3++;
+			totEleit++;
+			strcpy(nome, "**ALBUQUERQUE**");
 		}else if(numCand == 4){
-			strcpy(nome,"Albuquerque");
-			qtCand4++; 
-			tEleit++;
-			
-		}else if(numCand == 0){
-			strcpy(nome,"Nulo"); 
-			qtNulo++; 
-			tEleit++;
-			
-		}else if(numCand <= -1){
-			break; //finaliza (interrompe o while)
-						
+			qtCand4++;
+			totEleit++;
+			strcpy(nome, "**UHLMANN**");
+		}else if(numCand < 0){ 
+			break; //finaliza
 		}else{
-			strcpy(nome,"Branco"); //nome = "Ortiz";
-			qtBran++; //qtCand1 = qtCand1 + 1;
-			tEleit++;
+			qtBran++;
+			totEleit++;
+			strcpy(nome, "**BRANCO**");	
 		}
 		
-		printf("\n\nVoto computado: ** %s **", nome);
+		printf("\n\n%s",nome);
 		_sleep(800);
-				
+		
 		system("cls");
-	}while(numCand > -1);
+	}while(numCand >= 0);
 	
-	//cálculos
-		pCand1 = ((float)qtCand1/(float)tEleit)*100;
-		pCand2 = ((float)qtCand2/(float)tEleit)*100;
-		pCand3 = ((float)qtCand3/(float)tEleit)*100;
-		pCand4 = ((float)qtCand4/(float)tEleit)*100;
-		pNulo = ((float)qtNulo/(float)tEleit)*100;
-		pBran = ((float)qtBran/(float)tEleit)*100;
+	//calculos
+	pCand1 = ((float)qtCand1/(float)totEleit)*100;
+	pCand2 = ((float)qtCand2/(float)totEleit)*100;
+	pCand3 = ((float)qtCand3/(float)totEleit)*100;
+	pCand4 = ((float)qtCand4/(float)totEleit)*100;
+	pBran = ((float)qtBran/(float)totEleit)*100;
+	pNulo = ((float)qtNulo/(float)totEleit)*100;
 	
-	//Resultados
+	//ApresentaÃ§Ã£o dos resultados
 	system("cls");
-	printf("***Resultados Finais***\n\n");
+	printf("**** RESULTADOS FINAIS ****\n\n");
 	
-	printf("ORTIZ: %d\n", qtCand1);
+	printf("ORTIZ: %d votos\n", qtCand1);
 	for(i=0; i<pCand1; i++){
 		printf("|");
+		_sleep(30);
 	}
-	printf(" (%.1f%%)\n\n",pCand1);
-	/***********************************/	
-	printf("ULHMANN: %d\n", qtCand2);
+	printf(" - %.1f%%\n\n",pCand1);
+	
+	/*****************************************/
+	printf("HEREDIA: %d votos\n", qtCand2);
 	for(i=0; i<pCand2; i++){
 		printf("|");
+		_sleep(30);
 	}
-	printf(" (%.1f%%)\n\n",pCand2);
-	/***********************************/	
-	printf("HEREDIA: %d\n", qtCand3);
+	printf(" - %.1f%%\n\n",pCand2);
+	
+	/*****************************************/
+	printf("ALBUQUERQUE: %d votos\n", qtCand3);
 	for(i=0; i<pCand3; i++){
 		printf("|");
+		_sleep(30);
 	}
-	printf(" (%.1f%%)\n\n",pCand3);
-	/***********************************/	
-	printf("ALBUQUERQUE: %d\n", qtCand4);
+	printf(" - %.1f%%\n\n",pCand3);
+	/*****************************************/
+	printf("ULHMANN: %d votos\n", qtCand4);
 	for(i=0; i<pCand4; i++){
 		printf("|");
+		_sleep(30);
 	}
-	printf(" (%.1f%%)\n\n",pCand4);
-	/***********************************/	
-	printf("Brancos: %d\n", qtBran);
+	printf(" - %.1f%%\n\n",pCand4);
+	/*****************************************/
+	printf("BRANCOS: %d votos\n", qtBran);
 	for(i=0; i<pBran; i++){
 		printf("|");
+		_sleep(30);
 	}
-	printf(" (%.1f%%)\n\n",pBran);
-	/***********************************/	
-	printf("Nulos: %d\n", qtNulo);
-	for(i=0; i<pBran; i++){
+	printf(" - %.1f%%\n\n",pBran);
+	/*****************************************/
+	printf("NULOS: %d votos\n", qtNulo);
+	for(i=0; i<pNulo; i++){
 		printf("|");
+		_sleep(30);
 	}
-	printf(" (%.1f%%)\n\n",pBran);
+	printf(" - %.1f%%\n\n",pNulo);
 	
-	
-	printf("\n\n");	
+		 
+	printf("\n\n");
 	system("pause");
+	 
 }
-
-
 
