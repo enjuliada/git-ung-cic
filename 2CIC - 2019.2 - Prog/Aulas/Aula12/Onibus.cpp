33a5245923e9a832f766e396b3481a728fdb5e8b
joaoortiz <48797718+joaoortiz@users.noreply.github.com>
2019-11-06 22:23:16 -0300

    Add files via upload

--- 2CIC - 2019.2 - Prog/Aulas/Aula12/Onibus.cpp
+++ 2CIC - 2019.2 - Prog/Aulas/Aula12/Onibus.cpp
@@ -7,6 +7,7 @@
 /**VARIAVEIS PUBLICAS**/
 int onibA[42], onibB[42], onibC[42];
 int i;
+float total, valor, caixa = 0;
 
 void esvaziarOnibus(){
 	for(i=0; i<42; i++){
@@ -18,8 +19,7 @@ void esvaziarOnibus(){
 
 void mostrarOnibus(int tmpOnib[]){
 	char status[10];
-	system("cls");
-	
+		
 	for(i=0; i<42; i++){
 		if(tmpOnib[i] == 0){
 				strcpy(status,"Vazio");
@@ -29,9 +29,9 @@ void mostrarOnibus(int tmpOnib[]){
 			
 		if(i % 2 == 0){				
 		
-			printf("POLTRONA[%d] = %s\t", i, status);		
+			printf("POLTRONA[%d] = %s\t", i+1, status);		
 		}else{
-			printf("POLTRONA[%d] = %s\n", i, status);
+			printf("POLTRONA[%d] = %s\n", i+1, status);
 		}		
 		
 	}//fechando for
@@ -56,10 +56,27 @@ int montarMenu(){
 	return menu;
 }
 
+bool efetuarVenda(int tmpOnib[], int tmpPolt){
+	if(tmpOnib[tmpPolt - 1] == 1){
+		return false;		
+	}else{
+		if(tmpPolt < 10){
+			total = valor * 1.25;
+		}else{
+			total = valor;
+		}
+			caixa += total;
+			printf("Total a pagar: R$ %.2f\n",total);
+			tmpOnib[tmpPolt - 1] = 1; //poltrona ocupada 
+			return true;
+		}		
+	}	
+
 int main(){
 
-	int opcao;
+	int opcao, polt;
 	char codOnibus;
+	bool statusVenda;
 	
 	setlocale(LC_ALL,"");
 	esvaziarOnibus();
@@ -72,20 +89,51 @@ int main(){
 		switch(opcao){
 			
 			case 1:
+				printf("Entre com o destino: ");
+				scanf("%s", &codOnibus);
+				printf("Entre com o número da poltrona:");
+				scanf("%d", &polt);
+				
+				if(codOnibus == 'a'){
+					valor = 75;
+					statusVenda = efetuarVenda(onibA,polt);					
+				}else if(codOnibus == 'b'){
+					valor = 90;
+					statusVenda = efetuarVenda(onibB,polt);					
+				}else if(codOnibus == 'c'){
+					valor = 250;
+					statusVenda = efetuarVenda(onibC,polt);					
+				}else{
+					printf("Destino Inválido.");
+					break;
+				}
+				
+				if(statusVenda == true)
+					printf("Venda Efetuada!!");
+				else
+					printf("Poltrona Ocupada!!");
+					
+				_sleep(1500);
 				//venda
 				break;
 			case 2:
+				system("cls");
 				printf("\nEntre com o código do onibus: ");
 				scanf("%s", &codOnibus);
-				
-				if(codOnibus == 'a')
+				printf("\n\n");				
+				if(codOnibus == 'a'){
+					printf("## ONIBUS A - Destino -> Rio de Janeiro ##\n\n");
 					mostrarOnibus(onibA);
-				else if	(codOnibus == 'b')
+				}else if(codOnibus == 'b'){
+					printf("## ONIBUS B - Destino -> Belo Horizonte ##\n\n");
 					mostrarOnibus(onibB);
-				else if	(codOnibus == 'c')
-					mostrarOnibus(onibB);
-				else
+				}else if(codOnibus == 'c'){
+					printf("## ONIBUS C - Destino -> Acre ##\n\n");
+					mostrarOnibus(onibC);
+				}else{
 					printf("Código Inválido.\n");					
+				}
+				
 				break;
 			case 3:
 				//relatorio
