024bbadb031930292a8444caf0bc3975e1f7fbaa
joaoortiz <48797718+joaoortiz@users.noreply.github.com>
2019-11-14 09:43:15 -0300

    Add files via upload

---
+++ 6CIC - 2019.2 - WEB/ProjectManagement/src/UI/NavTopoUI.php
@@ -0,0 +1,18 @@
+ <nav class="navbar navbar-expand-lg bg-primary-dark">
+
+            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                <div class="nav navbar-brand">
+                    <img src="../../img/logo.png">
+                </div>
+                <div class="navbar-nav col-md-10">
+                    <a class="navbar-brand text-white" href="HomeUsuariosUI.php">Sistema de Gerenciamento de Projetos</a>
+                    <a class="nav-item nav-link text-white" href="ListaProjetosUI.php">Meus Projetos</a>
+                    <a class="nav-item nav-link text-white" href="../Control/UsuariosControl.php?acao=3">Sair</a>
+                </div>
+                <div class="navbar-nav col-md-2">                    
+                    <h5 class="float-right text-white"><?=$_SESSION['nome'];?></h5>
+                </div>
+                    
+                </div>
+            </div>
+        </nav>
