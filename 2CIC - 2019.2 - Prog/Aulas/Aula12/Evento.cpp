db785c846cab492078a4f91f4f4ce02838211c47
joaoortiz <48797718+joaoortiz@users.noreply.github.com>
2019-11-19 08:46:34 -0300

    Add files via upload

--- 2CIC - 2019.2 - Prog/Aulas/Aula12/Evento.cpp
+++ 2CIC - 2019.2 - Prog/Aulas/Aula12/Evento.cpp
@@ -7,6 +7,7 @@
 
 int poltronas[250];
 int i;
+float caixa;
 
 void esvaziarPoltronas(){
 	for(i=0; i<250; i++){
@@ -22,7 +23,7 @@ void exibirMapa(){
 		if(i==0) 
 			printf("## SETOR VIP ##\n\n");
 		else if(i==50){
-			printf("\n\n\a");
+			printf("\n\n");
 			system("pause");
 			printf("\n\n## SETOR COMUM ##\n\n");
 		}else if(i==150){
@@ -73,9 +74,51 @@ int montarMenu(){
 	
 }//fechando montarMenu
 
+int efetuarVenda(int tmpPoltrona){
+	float preco;
+	
+	if(poltronas[tmpPoltrona-1] == 1){
+		return -1; //polt. ocup.
+		
+	}else if(tmpPoltrona<1 || tmpPoltrona>250){
+		return -2; //polt. inex.
+		
+	}else{ //fazer venda
+		if(tmpPoltrona>=0 && tmpPoltrona<=49){
+			preco = 300;
+		}else if(tmpPoltrona<=149){
+			preco = 200;			
+		}else{
+			preco = 100;
+		}
+		
+		caixa += preco; //caixa = caixa + preco;
+		poltronas[tmpPoltrona-1] = 1;//ocupando poltrona
+		
+		return 1;
+		
+	} //fechando else princ.
+	
+} //fechando efetuarVenda
+
+void menuVenda(){
+	int statusVenda, nPolt, setor;
+	
+	system("cls");
+	printf("## PROCESSO DE VENDA DE INGRESSO ##\n\n");
+	printf("Entre com o setor:\n\n");
+	printf("1 - Setor Vip\n");
+	printf("2 - Setor Comum\n");
+	printf("3 - Setor Superior\n\n");
+	printf("Setor: ");
+	scanf("%d", &setor);	
+	
+}
+
 int main(){
 
 	int menu;
+	
 	setlocale(LC_ALL,"");
 	
 	esvaziarPoltronas();
@@ -86,7 +129,7 @@ int main(){
 		
 		switch(menu){
 			case 1:
-				//efetuar venda
+				//efetuar venda				
 				break;
 			
 			case 2:
