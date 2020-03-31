8af7d2749f9cb8357637423b252b555cf920cd9d
joaoortiz <48797718+joaoortiz@users.noreply.github.com>
2020-03-31 08:53:11 -0300

    Add files via upload

--- 3CIC - 2020.1 - Prog. Orient/Sistema Shopping/src/LojasVO.java
+++ 3CIC - 2020.1 - Prog. Orient/Sistema Shopping/src/LojasVO.java
@@ -5,28 +5,49 @@ public class LojasVO {
     private int lojas[] = new int[40]; //vetor de 40 posições
     private String nomes[] = new String[40]; //vetor de 40 posições
     private float faturamento;
-    
+
     //método construtor
-    public LojasVO(){
+    public LojasVO() {
+
+        
         
     }
-    
+
     //métodos funcionais
-    
-    public int alocar(int andar, int numero, String nomeLoja){
+    public int alocar(int andar, int numero, String nomeLoja) {
         int posicao;
         posicao = (andar * 10) + (numero - 1);
-        
-        
-        if(numero > 10){
+
+        if (numero > 10) {
             return -1; //posição 'loja' invalida
-        }else if(posicao > 39){
+        } else if (posicao > 39) {
             return -1; //posição 'loja' invalida
-        }else if(lojas[posicao] == 1){
+        } else if (lojas[posicao] == 1) {
             return -2; //espaço ocupado
-        }else{
+        } else {
+            nomes[posicao] = nomeLoja;
+            lojas[posicao] = 1; //ocupando espaço vazio (deixa de ser 0 e se torna 1)
+            atualizarFaturamento(andar);
             return 1; // ok
         }
     }//fechando alocar
-    
+
+    public void atualizarFaturamento(int andar) {
+        switch (andar) {
+            case 0:
+                faturamento += 8000;
+                break;
+            case 1:
+                faturamento += 9500;
+                break;
+            case 2:
+                faturamento += 12000;
+                break;
+            case 3:
+                faturamento += 15000;
+                break;
+        }
+
+    }//fechando atualizarFaturamento
+
 }//fechando class
