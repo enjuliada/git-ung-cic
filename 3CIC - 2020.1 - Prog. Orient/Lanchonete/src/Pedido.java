91bc8af76247f87540e33cdce8b4e7f41e6d2f56
joaoortiz <48797718+joaoortiz@users.noreply.github.com>
2020-05-19 10:06:54 -0300

    Add files via upload

--- 3CIC - 2020.1 - Prog. Orient/Lanchonete/src/Pedido.java
+++ 3CIC - 2020.1 - Prog. Orient/Lanchonete/src/Pedido.java
@@ -1,15 +1,61 @@
+import java.util.*; //Uso da ArrayList
+
 public class Pedido {
     
     public String lanches[]={"Big-Mac","Cheddar McMelt",
-                           "Mc-Chicken", "Quarteirão",
-                           "Mc-Fish", "Big-Tasty",
-                           "Angus Bacon", "MC-Salad"};
+                             "Mc-Chicken", "Quarteirão",
+                             "Mc-Fish", "Big-Tasty",
+                             "Angus Bacon", "MC-Salad"};
+    
+    public float vLanches[] = {(float)9.5, (float)8,
+                               (float)6.5, (float)8,
+                               (float)5, (float)14.5,
+                               (float)16, (float)12.5};
     
     public String acomp[]={"Mc-Fritas","Mc-Nuggets",
                            "Regriferante", "Água",
                            "Casquinha", "Top-Sundae",
                            "Mc-Flurry", "Milk-Shake"};
     
+    public float vAcomp[] = {(float)5.5, (float)7,
+                             (float)5, (float)3,
+                             (float)2.8, (float)6.5,
+                             (float)10, (float)12};
     
+    public float vTotal;
     
-}
+    public List<Item> lista = new ArrayList<Item>();
+    
+    public Pedido(){
+        vTotal = 0;
+    }
+    
+    public List adicionarItem(int idItem){
+
+        Item itemAtual = new Item();
+        
+        itemAtual.setId(idItem);
+        itemAtual.setNome(lanches[idItem]);
+        itemAtual.setValor(vLanches[idItem]);        
+        itemAtual.setQtde(1);        
+        //if(qtde)
+        
+        lista.add(itemAtual);
+        
+        return lista;
+        
+    }
+    
+    public float atualizarPedido(int tmpTipo, int idItem){
+        
+        if(tmpTipo == 1){ //lanche
+            vTotal += vLanches[idItem];            
+        }else if(tmpTipo == 2){ //acomp
+            vTotal += vAcomp[idItem];
+        }
+        
+        return vTotal;
+    
+    }//fechando atualizarPedido    
+    
+}//fechando Classe
--- 3CIC - 2020.1 - Prog. Orient/Lanchonete/src/View.java
+++ 3CIC - 2020.1 - Prog. Orient/Lanchonete/src/View.java
@@ -1,71 +1,141 @@
+
 import javax.swing.*;
 import javax.swing.table.*;
 import java.awt.*;
 import java.awt.event.*;
+import java.util.*;
 
-public class View extends JFrame implements ActionListener{
-    
+public class View extends JFrame implements ActionListener {
+
     //1 - Declaração dos objetos
     public static Container ctnTela;
     public static ImageIcon imgLanches[], imgAcomp[];
-    public static JButton btnLanches[], btnAcomp[];
-        
-    public static JLabel lblLanches, lblAcomp;
-    
+    public static JButton btnLanches[], btnAcomp[], btnFecharPedido;
+
+    public static JLabel lblLanches, lblAcomp, lblBanner, lblPedido, lblValorPedido, lblTotal, lblValorTotal;
+    public static Font fntTexto = new Font("Verdana", Font.BOLD, 22);
+
     public static JTable tblItens; //tabela
     public static JScrollPane scrItens; //barra de rolagem da tabela
     public static DefaultTableModel mdlItens; //gerenciar itens da tabela
-        
-    
-    public View(){ //construtor
-        
+
+    public static float total;
+
+    public static java.util.List<Item> lista = new ArrayList<Item>();
+
+    public View() { //construtor
+
         super("Aplicação Lanchonete - 3CIC"); //barra de titulo da janela
-        
+
         //2 - Construção dos objetos (inst. e config)
         ctnTela = new Container();
         ctnTela.setLayout(null);
         this.add(ctnTela); //add Container na Janela
-        
+
+        lblBanner = new JLabel(new ImageIcon("img/banner.jpg"));
+        lblBanner.setBounds(15, 0, 450, 100);
+        ctnTela.add(lblBanner);
+
+        lblPedido = new JLabel("Valor do Pedido:");
+        lblPedido.setFont(fntTexto);
+        lblPedido.setBounds(15, 125, 250, 25);
+        ctnTela.add(lblPedido);
+
+        lblValorPedido = new JLabel("R$ 0,00");
+        lblValorPedido.setFont(fntTexto);
+        lblValorPedido.setBounds(240, 125, 150, 25);
+        ctnTela.add(lblValorPedido);
+
+        btnFecharPedido = new JButton("Fechar Pedido");
+        btnFecharPedido.addActionListener(this);
+        btnFecharPedido.setBounds(15, 170, 450, 40);
+        ctnTela.add(btnFecharPedido);
+
+        tblItens = new JTable();
+        scrItens = new JScrollPane(tblItens);
+        mdlItens = (DefaultTableModel) tblItens.getModel();
+
+        String legenda[] = {"Item", "Qtde"};
+        for (int i = 0; i < legenda.length; i++) {
+            mdlItens.addColumn(legenda[i]);
+        }
+
+        scrItens.setBounds(500, 20, 280, 200);
+        ctnTela.add(scrItens);
+
         lblLanches = new JLabel("Lanches");
-        lblLanches.setBounds(360,200,100,20);
+        lblLanches.setBounds(360, 230, 100, 20);
         ctnTela.add(lblLanches);
-        
+
         lblAcomp = new JLabel("Acompanhamentos");
-        lblAcomp.setBounds(330,350,150,20);
+        lblAcomp.setBounds(330, 380, 150, 20);
         ctnTela.add(lblAcomp);
-        
+
         imgLanches = new ImageIcon[8];
         btnLanches = new JButton[8];
         imgAcomp = new ImageIcon[8];
         btnAcomp = new JButton[8];
-        for(int i=0; i<btnLanches.length; i++){
+
+        for (int i = 0; i < btnLanches.length; i++) {
             imgLanches[i] = new ImageIcon("img/L" + i + ".jpg");
             btnLanches[i] = new JButton(imgLanches[i]);
-            btnLanches[i].setBounds(25 + (95*i),235, 80, 80);
+            btnLanches[i].addActionListener(this);
+            btnLanches[i].setToolTipText(Control.objPedido.lanches[i]);
+            btnLanches[i].setBounds(25 + (95 * i), 265, 80, 80);
             ctnTela.add(btnLanches[i]);
-            
+
             imgAcomp[i] = new ImageIcon("img/A" + i + ".jpg");
-            btnAcomp[i] = new JButton(Control.objPedido.acomp[i]);
-            btnAcomp[i].setBounds(25 + (95*i),385, 80, 80);
-            ctnTela.add(btnAcomp[i]);            
+            btnAcomp[i] = new JButton(imgAcomp[i]);
+            btnAcomp[i].addActionListener(this);
+            btnAcomp[i].setToolTipText(Control.objPedido.acomp[i]);
+            btnAcomp[i].setBounds(25 + (95 * i), 415, 80, 80);
+            ctnTela.add(btnAcomp[i]);
         }
-        
-        
-        
-        
-        
-        
-        
+
         //3 - Configuração da Janela
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setResizable(false);
-        this.setSize(800,600);
+        this.setSize(800, 600);
         this.setVisible(true);
-        
+
+    }
+
+    public void actionPerformed(ActionEvent evt) { //ação em botões
+
+        for (int i = 0; i < btnLanches.length; i++) {
+            if (evt.getSource() == btnLanches[i]) {
+
+                total = Control.objPedido.atualizarPedido(1, i);
+                lblValorPedido.setText("R$ " + total);
+                lista = Control.objPedido.adicionarItem(i);
+                atualizarTabela();
+
+            } else if (evt.getSource() == btnAcomp[i]) {
+
+                total = Control.objPedido.atualizarPedido(2, i);
+                lblValorPedido.setText("R$ " + total);
+                lista = Control.objPedido.adicionarItem(i);
+                atualizarTabela();
+
+            }
+        }
+
     }
     
-    public void actionPerformed(ActionEvent evt){ //ação em botões
+    public void atualizarTabela(){
+        
+        while(mdlItens.getRowCount()>0){ //limpando tabela
+            mdlItens.removeRow(0);
+        }
+        
+        for(Item itemAtual: lista){
+            String teste[] = new String[2];
+            teste[0] = itemAtual.getNome();
+            teste[1] = "" + itemAtual.getQtde();
+                        
+            mdlItens.addRow(teste);
+        }
         
     }
-    
+
 }//fechando classe
